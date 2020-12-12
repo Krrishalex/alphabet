86993591c6165e8833b5064d2bb948c0fe0ff5a2
Krrishalex <chemikelak999@gmail.com>
2020-12-12 13:11:59 +0530

    surendra commit

---
+++ alphabet/src/radio_buttons/TestCheckBox.java
@@ -0,0 +1,11 @@
+package radio_buttons;
+
+public class TestCheckBox {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Check box");
+
+	}
+
+}
--- alphabet/src/radio_buttons/TestRadio.java
+++ alphabet/src/radio_buttons/TestRadio.java
@@ -5,6 +5,7 @@ public class TestRadio {
 	public static void main(String[] args) {
 		
 		System.out.println("radio button clicked");
+		System.out.println("Hello I'm updated");
 
 	}
 
